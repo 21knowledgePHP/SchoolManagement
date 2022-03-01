06ed6678b97bfc2b3470086fff9c561fe7530503
21knowledgePHP <mohamedamine11@gmail.com>
2022-03-01 14:06:06 +0100

    Update app.php

--- app.php
+++ app.php
@@ -15,6 +15,10 @@ $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
+$app2 = new Illuminate\Foundation\Application(
+    $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
+);
+
 /*
 |--------------------------------------------------------------------------
 | Bind Important Interfaces
